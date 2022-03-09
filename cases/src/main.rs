0c9806a737fa63f1957335442bafb28cbd251000
Joii <joii@cryptape.com>
2022-03-09 16:38:28 +0800

    Add test case: vcompress.vm

--- cases/src/main.rs
+++ cases/src/main.rs
@@ -23,6 +23,7 @@ mod single_width_averaging_cases;
 mod single_width_integer_multiply_add_cases;
 mod single_width_integer_reduction_cases;
 mod single_width_shift_cases;
+mod vector_compress_cases;
 mod vector_element_index_cases;
 mod vector_iota_cases;
 
@@ -131,5 +132,6 @@ fn program_entry(argc: u64, argv: *const *const u8) -> i8 {
     test_case!(load_store_cases::test_load_store, test_pattern);
     test_case!(integer_merge_cases::test_integer_merge, test_pattern);
     test_case!(adc_sbc_cases::test_adc_sbc, test_pattern);
+    test_case!(vector_compress_cases::test_vector_compress, test_pattern);
     0
 }
