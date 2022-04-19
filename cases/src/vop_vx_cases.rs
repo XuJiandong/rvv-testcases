e6e6b7a27f35ab7e8cb67a800fc5123150cf757f
Joii <joii@cryptape.com>
2022-04-19 18:28:07 +0800

    Add vsub.vx testcase

--- cases/src/vop_vx_cases.rs
+++ cases/src/vop_vx_cases.rs
@@ -37,6 +37,38 @@ fn expected_op_add(lhs: &[u8], x: u64, result: &mut [u8]) {
     }
 }
 
+fn expected_op_sub(lhs: &[u8], x: u64, result: &mut [u8]) {
+    assert_eq!(lhs.len(), result.len());
+    match lhs.len() {
+        1 => {
+            result[0] = lhs[0] - x as u8;
+        }
+        2 => {
+            let r = u16::from_le_bytes(lhs.try_into().unwrap()) - x as u16;
+            result.copy_from_slice(&r.to_le_bytes());
+        }
+        4 => {
+            let r = u32::from_le_bytes(lhs.try_into().unwrap()) - x as u32;
+            result.copy_from_slice(&r.to_le_bytes());
+        }
+        8 => {
+            let r = u64::from_le_bytes(lhs.try_into().unwrap()) - x as u64;
+            result.copy_from_slice(&r.to_le_bytes());
+        }
+        16 => {
+            let r = u128::from_le_bytes(lhs.try_into().unwrap()) - x as u128;
+            result.copy_from_slice(&r.to_le_bytes());
+        }
+        32 => {
+            let (r, _) = U256::from_little_endian(lhs).overflowing_sub(x.sign_extend());
+            r.to_little_endian(result);
+        }
+        _ => {
+            panic!("Invalid sew");
+        }
+    }
+}
+
 pub fn test_vop_vx() {
     // test combinations of lmul, sew, avl, etc
     fn add(lhs: &[u8], x: u64, result: &mut [u8], sew: u64, lmul: i64, avl: u64) {
@@ -57,6 +89,28 @@ pub fn test_vop_vx() {
                 add,
                 WideningCategory::None,
                 "vadd.vx",
+            );
+        }
+    }
+
+    fn sub(lhs: &[u8], x: u64, result: &mut [u8], sew: u64, lmul: i64, avl: u64) {
+        vop_vx(lhs, x, result, sew, avl, lmul, |x: u64| unsafe {
+            rvv_asm!("mv t0, {}", 
+                     "vsub.vx v24, v8, t0",
+                     in (reg) x);
+        });
+    }
+    let sew = 256u64;
+    for lmul in [-2, 1, 4, 8] {
+        for avl in avl_iterator(sew, 4) {
+            run_vop_vx(
+                sew,
+                lmul,
+                avl,
+                expected_op_sub,
+                sub,
+                WideningCategory::None,
+                "vsub.vx",
             );
         }
     }
