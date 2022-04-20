56f09f80db874650927f5019136dff562c83cf12
Joii <joii@cryptape.com>
2022-04-20 16:28:45 +0800

    Add vsra.vv vsra.vx vsra.vi testcase

--- cases/src/single_width_shift_cases.rs
+++ cases/src/single_width_shift_cases.rs
@@ -71,6 +71,38 @@ fn test_vsrl_vi(sew: u64, lmul: i64, avl: u64) {
     );
 }
 
+fn expected_op_sra_vi(lhs: &[u8], _: u64, result: &mut [u8]) {
+    assert_eq!(lhs.len(), result.len());
+
+    match result.len() {
+        32 => {
+            let l = E256::get(lhs);
+            let res = l.wrapping_sra(10);
+            res.put(result);
+        }
+        n => {
+            panic!("Invalid sew: {}", n);
+        }
+    }
+}
+
+fn test_vsra_vi(sew: u64, lmul: i64, avl: u64) {
+    fn op(lhs: &[u8], x: u64, result: &mut [u8], sew: u64, lmul: i64, avl: u64) {
+        vop_vx(lhs, x, result, sew, avl, lmul, |_| unsafe {
+            rvv_asm!("vsra.vi v24, v8, 10");
+        });
+    }
+    run_vop_vx(
+        sew,
+        lmul,
+        avl,
+        expected_op_sra_vi,
+        op,
+        WideningCategory::None,
+        "vsra.vi",
+    );
+}
+
 fn expected_op_vv(lhs: &[u8], rhs: &[u8], result: &mut [u8]) {
     assert_eq!(lhs.len(), result.len());
 
@@ -107,6 +139,39 @@ fn test_vsll_vv(sew: u64, lmul: i64, avl: u64) {
         sll_vv,
         WideningCategory::None,
         "vsll.vv",
+    );
+}
+
+fn expected_op_sra_vv(lhs: &[u8], rhs: &[u8], result: &mut [u8]) {
+    assert_eq!(lhs.len(), result.len());
+
+    match result.len() {
+        32 => {
+            let l = E256::get(lhs);
+            let r = E256::get(rhs);
+            let res = l.wrapping_sra(r.u32());
+            res.put(result);
+        }
+        n => {
+            panic!("Invalid sew: {}", n);
+        }
+    }
+}
+
+fn test_vsra_vv(sew: u64, lmul: i64, avl: u64) {
+    fn op(lhs: &[u8], rhs: &[u8], result: &mut [u8], sew: u64, lmul: i64, avl: u64) {
+        vop_vv(lhs, rhs, result, sew, avl, lmul, || unsafe {
+            rvv_asm!("vsra.vv v24, v8, v16");
+        });
+    }
+    run_vop_vv(
+        sew,
+        lmul,
+        avl,
+        ExpectedOp::Normal(Box::new(expected_op_sra_vv)),
+        op,
+        WideningCategory::None,
+        "vsra.vv",
     );
 }
 
@@ -188,9 +253,6 @@ fn test_vsll_vx(sew: u64, lmul: i64, avl: u64) {
 fn expected_op_vsrl_vx(lhs: &[u8], x: u64, result: &mut [u8]) {
     assert_eq!(lhs.len(), result.len());
     match lhs.len() {
-        1 => {
-            result[0] = lhs[0] + x as u8;
-        }
         32 => {
             let l = E256::get(lhs);
             let res = l.wrapping_shr(x as u32);
@@ -221,6 +283,39 @@ fn test_vsrl_vx(sew: u64, lmul: i64, avl: u64) {
     );
 }
 
+fn expected_op_vsra_vx(lhs: &[u8], x: u64, result: &mut [u8]) {
+    assert_eq!(lhs.len(), result.len());
+    match lhs.len() {
+        32 => {
+            let l = E256::get(lhs);
+            let res = l.wrapping_sra(x as u32);
+            res.put(result);
+        }
+        _ => {
+            panic!("Invalid sew");
+        }
+    }
+}
+
+fn test_vsra_vx(sew: u64, lmul: i64, avl: u64) {
+    fn op(lhs: &[u8], x: u64, result: &mut [u8], sew: u64, lmul: i64, avl: u64) {
+        vop_vx(lhs, x, result, sew, avl, lmul, |x: u64| unsafe {
+            rvv_asm!("mv t0, {}", 
+                     "vsra.vx v24, v8, t0",
+                     in (reg) x);
+        });
+    }
+    run_vop_vx(
+        sew,
+        lmul,
+        avl,
+        expected_op_vsra_vx,
+        op,
+        WideningCategory::None,
+        "vsra.vx",
+    );
+}
+
 pub fn test_single_width_shift() {
     let sew = 256u64;
     for lmul in [-2, 1, 4, 8] {
@@ -232,6 +327,10 @@ pub fn test_single_width_shift() {
             test_vsrl_vi(sew, lmul, avl);
             test_vsrl_vv(sew, lmul, avl);
             test_vsrl_vx(sew, lmul, avl);
+
+            test_vsra_vi(sew, lmul, avl);
+            test_vsra_vv(sew, lmul, avl);
+            test_vsra_vx(sew, lmul, avl);
         }
     }
 }
