46c89aeeafd676f5a61c2d63ab5e2f999966194b
Joii <joii@cryptape.com>
2022-04-21 18:35:32 +0800

    Fix vzext.vf2 about ckb-vm checked

--- cases/src/integer_extension_cases.rs
+++ cases/src/integer_extension_cases.rs
@@ -4,17 +4,17 @@ use core::convert::TryInto;
 use alloc::boxed::Box;
 use rvv_asm::rvv_asm;
 use rvv_testcases::intrinsic::vop_nv;
-use rvv_testcases::misc::{avl_iterator, U256};
+use rvv_testcases::misc::{avl_iterator};
 use rvv_testcases::runner::{run_vop_vv, ExpectedOp, WideningCategory};
 
 fn expected_op(lhs: &[u8], _: &[u8], result: &mut [u8]) {
     assert_eq!(lhs.len(), result.len() / 2);
 
     match result.len() {
-        32 => {
-            let l = u128::from_le_bytes(lhs.try_into().unwrap());
-            let r: U256 = l.into();
-            r.to_little_endian(result);
+        16 => {
+            let l = u64::from_le_bytes(lhs.try_into().unwrap());
+            let r: u128 = l.into();
+            result.copy_from_slice(&r.to_le_bytes());
         }
         n => {
             panic!("Invalid sew: {}", n);
@@ -37,11 +37,11 @@ pub fn test_integer_extension() {
             WideningCategory::NarrowVs2(2),
         );
     }
-    let sew = 256u64;
+    let sew = 128u64;
     for lmul in [-2, 1, 4, 8] {
         for avl in avl_iterator(sew, 4) {
             run_vop_vv(
-                256,
+                sew,
                 lmul,
                 avl,
                 ExpectedOp::Normal(Box::new(expected_op)),
