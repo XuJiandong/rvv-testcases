5efdbd4793cdfcc500fc5b2283e2b6dd5e403be0
Joii <joii@cryptape.com>
2022-04-27 15:05:13 +0800

    Add width integer multiply add testcases vwmacc.vv vwmacc.vx vwmaccu.vx vwmaccsu.vx vwmaccus.vx

--- cases/src/single_width_integer_multiply_add_cases.rs
+++ cases/src/single_width_integer_multiply_add_cases.rs
@@ -1,13 +1,15 @@
-#![allow(dead_code)]
 use alloc::boxed::Box;
 use core::{arch::asm, convert::TryInto};
 use eint::{Eint, E256, E512};
 use rvv_asm::rvv_asm;
 use rvv_testcases::{
-    intrinsic::{vop_vv_destructive, vop_vv_destructive_wide},
+    intrinsic::{vop_vv_destructive, vop_vv_destructive_wide, vwop_vx},
     misc::{avl_iterator, conver_to_i512},
-    runner::{run_vop_vv, ExpectedOp, WideningCategory},
+    runner::{run_vop_vv, run_vop_vx, ExpectedOp, WideningCategory},
 };
+
+// use ckb_std::syscalls::debug;
+// use rvv_testcases::log;
 
 fn expected_op_macc_vv(lhs: &[u8], rhs: &[u8], result: &mut [u8]) {
     assert!(lhs.len() == rhs.len() && rhs.len() == result.len());
@@ -35,7 +37,7 @@ fn expected_op_macc_vv(lhs: &[u8], rhs: &[u8], result: &mut [u8]) {
         }
     }
 }
-fn test_vwmacc_vv(sew: u64, lmul: i64, avl: u64) {
+fn test_vmacc_vv(sew: u64, lmul: i64, avl: u64) {
     fn macc(lhs: &[u8], rhs: &[u8], result: &mut [u8], sew: u64, lmul: i64, avl: u64) {
         vop_vv_destructive(lhs, rhs, result, sew, avl, lmul, || unsafe {
             rvv_asm!("vmacc.vv v24, v8, v16");
@@ -52,7 +54,7 @@ fn test_vwmacc_vv(sew: u64, lmul: i64, avl: u64) {
     );
 }
 
-fn expected_op_maccu_vv(lhs: &[u8], rhs: &[u8], result: &mut [u8]) {
+fn expected_op_wmaccu_vv(lhs: &[u8], rhs: &[u8], result: &mut [u8]) {
     assert!(lhs.len() == rhs.len() && rhs.len() * 2 == result.len());
     match lhs.len() {
         2 => {
@@ -105,33 +107,162 @@ fn test_vwmaccu_vv(sew: u64, lmul: i64, avl: u64) {
         sew,
         lmul,
         avl,
-        ExpectedOp::Normal(Box::new(expected_op_maccu_vv)),
+        ExpectedOp::Normal(Box::new(expected_op_wmaccu_vv)),
         op,
         WideningCategory::VdOnly,
         "vwmaccu.vv",
     );
 }
 
-fn expected_op_maccsu_vv(lhs: &[u8], rhs: &[u8], result: &mut [u8]) {
+fn expected_op_wmaccu_vx(lhs: &[u8], rhs: u64, result: &mut [u8]) {
+    assert!(lhs.len() * 2 == result.len());
+    match lhs.len() {
+        8 => {
+            let l: u128 = u64::from_le_bytes(lhs.try_into().unwrap()).into();
+            let r: u128 = rhs.into();
+            let extra: u128 = u128::from_le_bytes(result.try_into().unwrap());
+
+            let res2 = l.wrapping_mul(r).wrapping_add(extra);
+            result.copy_from_slice(&res2.to_le_bytes());
+        }
+        32 => {
+            let l = E512::from(E256::get(lhs));
+            let r = E512::from(rhs);
+            let extra = E512::get(result);
+
+            let (res, _) = l.overflowing_mul_u(r);
+            let (res2, _) = res.overflowing_add_u(extra);
+            res2.put(result);
+        }
+        _ => {
+            panic!("Invalid sew");
+        }
+    }
+}
+fn test_vwmaccu_vx(sew: u64, lmul: i64, avl: u64) {
+    fn op(lhs: &[u8], rhs: u64, result: &mut [u8], sew: u64, lmul: i64, avl: u64) {
+        vwop_vx(lhs, rhs, result, sew, avl, lmul, |rhs: u64| unsafe {
+            rvv_asm!("mv t0, {}", "vwmaccu.vx v24, v8, t0", in (reg) rhs);
+        });
+    }
+    run_vop_vx(
+        sew,
+        lmul,
+        avl,
+        expected_op_wmaccu_vx,
+        op,
+        WideningCategory::VdOnly,
+        "vwmaccu.vx",
+    );
+}
+
+fn expected_op_wmacc_vv(lhs: &[u8], rhs: &[u8], result: &mut [u8]) {
     assert!(lhs.len() == rhs.len() && rhs.len() * 2 == result.len());
     match lhs.len() {
         8 => {
             let l: i128 = i64::from_le_bytes(lhs.try_into().unwrap()).into();
-            let r: i128 = u64::from_le_bytes(rhs.try_into().unwrap()).into();
+            let r: i128 = i64::from_le_bytes(rhs.try_into().unwrap()).into();
 
-            let extra: i128 = i128::from_le_bytes(result.try_into().unwrap());
+            let extra = i128::from_le_bytes(result.try_into().unwrap());
 
             let res2 = l.wrapping_mul(r).wrapping_add(extra);
             result.copy_from_slice(&res2.to_le_bytes());
         }
         32 => {
             let l = conver_to_i512(E256::get(lhs));
-            let r = E512::from(E256::get(rhs));
+            let r = conver_to_i512(E256::get(rhs));
 
             let extra = E512::get(result);
 
-            let (res, _) = l.overflowing_mul_u(r);
-            let (res2, _) = res.overflowing_add_u(extra);
+            let (res, _) = l.overflowing_mul_s(r);
+            let (res2, _) = res.overflowing_add_s(extra);
+            res2.put(result);
+        }
+        _ => {
+            panic!("Invalid sew");
+        }
+    }
+}
+fn test_vwmacc_vv(sew: u64, lmul: i64, avl: u64) {
+    fn op(lhs: &[u8], rhs: &[u8], result: &mut [u8], sew: u64, lmul: i64, avl: u64) {
+        vop_vv_destructive_wide(lhs, rhs, result, sew, avl, lmul, || unsafe {
+            rvv_asm!("vwmacc.vv v24, v8, v16");
+        });
+    }
+    run_vop_vv(
+        sew,
+        lmul,
+        avl,
+        ExpectedOp::Normal(Box::new(expected_op_wmacc_vv)),
+        op,
+        WideningCategory::VdOnly,
+        "vwmacc.vv",
+    );
+}
+
+fn expected_op_wmacc_vx(lhs: &[u8], rhs: u64, result: &mut [u8]) {
+    assert!(lhs.len() * 2 == result.len());
+    match lhs.len() {
+        8 => {
+            let l: i128 = i64::from_le_bytes(lhs.try_into().unwrap()).into();
+            let r: i128 = (rhs as i64) as i128;
+
+            let extra = i128::from_le_bytes(result.try_into().unwrap());
+
+            let res2 = l.wrapping_mul(r).wrapping_add(extra);
+            result.copy_from_slice(&res2.to_le_bytes());
+        }
+        32 => {
+            let l = conver_to_i512(E256::get(lhs));
+            let r = E512::from(rhs as i64);
+
+            let extra = E512::get(result);
+
+            let (res, _) = l.overflowing_mul_s(r);
+            let (res2, _) = res.overflowing_add_s(extra);
+            res2.put(result);
+        }
+        _ => {
+            panic!("Invalid sew");
+        }
+    }
+}
+fn test_vwmacc_vx(sew: u64, lmul: i64, avl: u64) {
+    fn op(lhs: &[u8], rhs: u64, result: &mut [u8], sew: u64, lmul: i64, avl: u64) {
+        vwop_vx(lhs, rhs, result, sew, avl, lmul, |rhs: u64| unsafe {
+            rvv_asm!("mv t0, {}", "vwmacc.vx v24, v8, t0", in (reg) rhs);
+        });
+    }
+    run_vop_vx(
+        sew,
+        lmul,
+        avl,
+        expected_op_wmacc_vx,
+        op,
+        WideningCategory::VdOnly,
+        "vwmacc.vv",
+    );
+}
+
+fn expected_op_wmaccsu_vv(lhs: &[u8], rhs: &[u8], result: &mut [u8]) {
+    assert!(lhs.len() == rhs.len() && rhs.len() * 2 == result.len());
+    match lhs.len() {
+        8 => {
+            let l = i64::from_le_bytes(lhs.try_into().unwrap()) as i128;
+            let r = u64::from_le_bytes(rhs.try_into().unwrap()) as i128;
+            let extra = i128::from_le_bytes(result.try_into().unwrap());
+
+            let res1 = l.wrapping_mul(r);
+            let res2 = res1.wrapping_add(extra);
+            result.copy_from_slice(&res2.to_le_bytes());
+        }
+        32 => {
+            let l = conver_to_i512(E256::get(lhs));
+            let r = E512::from(E256::get(rhs));
+            let extra = E512::get(result);
+
+            let (res, _) = l.overflowing_mul_s(r);
+            let (res2, _) = res.overflowing_add_s(extra);
             res2.put(result);
         }
         _ => {
@@ -149,19 +280,114 @@ fn test_vwmaccsu_vv(sew: u64, lmul: i64, avl: u64) {
         sew,
         lmul,
         avl,
-        ExpectedOp::Normal(Box::new(expected_op_maccu_vv)),
+        ExpectedOp::Normal(Box::new(expected_op_wmaccsu_vv)),
         op,
         WideningCategory::VdOnly,
         "vwmaccsu.vv",
     );
 }
 
+fn expected_op_wmaccsu_vx(lhs: &[u8], rhs: u64, result: &mut [u8]) {
+    assert!(lhs.len() * 2 == result.len());
+    match lhs.len() {
+        8 => {
+            let l = i64::from_le_bytes(lhs.try_into().unwrap()) as i128;
+            let r = rhs as i128;
+            let extra = i128::from_le_bytes(result.try_into().unwrap());
+
+            let res1 = l.wrapping_mul(r);
+            let res2 = res1.wrapping_add(extra);
+            result.copy_from_slice(&res2.to_le_bytes());
+        }
+        32 => {
+            let l = conver_to_i512(E256::get(lhs));
+            let r = E512::from(rhs);
+            let extra = E512::get(result);
+
+            let (res, _) = l.overflowing_mul_s(r);
+            let (res2, _) = res.overflowing_add_s(extra);
+            res2.put(result);
+        }
+        _ => {
+            panic!("Invalid sew");
+        }
+    }
+}
+fn test_vwmaccsu_vx(sew: u64, lmul: i64, avl: u64) {
+    fn op(lhs: &[u8], rhs: u64, result: &mut [u8], sew: u64, lmul: i64, avl: u64) {
+        vwop_vx(lhs, rhs, result, sew, avl, lmul, |rhs: u64| unsafe {
+            rvv_asm!("mv t0, {}", "vwmaccsu.vx v24, v8, t0", in (reg) rhs);
+        });
+    }
+    run_vop_vx(
+        sew,
+        lmul,
+        avl,
+        expected_op_wmaccsu_vx,
+        op,
+        WideningCategory::VdOnly,
+        "vwmaccsu.vv",
+    );
+}
+
+fn expected_op_wmaccus_vx(lhs: &[u8], rhs: u64, result: &mut [u8]) {
+    assert!(lhs.len() * 2 == result.len());
+    match lhs.len() {
+        8 => {
+            let l = u64::from_le_bytes(lhs.try_into().unwrap()) as i128;
+            let r = (rhs as i64) as i128;
+            let extra = i128::from_le_bytes(result.try_into().unwrap());
+
+            let res1 = l.wrapping_mul(r);
+            let res2 = res1.wrapping_add(extra);
+            result.copy_from_slice(&res2.to_le_bytes());
+        }
+        32 => {
+            let l = E512::from(E256::get(lhs));
+            let r = E512::from(rhs as i64);
+            let extra = E512::get(result);
+
+            let (res, _) = l.overflowing_mul_s(r);
+            let (res2, _) = res.overflowing_add_s(extra);
+            res2.put(result);
+        }
+        _ => {
+            panic!("Invalid sew");
+        }
+    }
+}
+fn test_vwmaccus_vx(sew: u64, lmul: i64, avl: u64) {
+    fn op(lhs: &[u8], rhs: u64, result: &mut [u8], sew: u64, lmul: i64, avl: u64) {
+        vwop_vx(lhs, rhs, result, sew, avl, lmul, |rhs: u64| unsafe {
+            rvv_asm!("mv t0, {}", "vwmaccus.vx v24, v8, t0", in (reg) rhs);
+        });
+    }
+    run_vop_vx(
+        sew,
+        lmul,
+        avl,
+        expected_op_wmaccus_vx,
+        op,
+        WideningCategory::VdOnly,
+        "vwmaccus.vv",
+    );
+}
+
 pub fn test_widening_width_multiply_add() {
     for sew in [64, 256] {
-        for lmul in [-8, -2, 1, 2] {
+        for lmul in [-4, -2, 1, 2, 4] {
             for avl in avl_iterator(sew, 4) {
-                test_vwmacc_vv(sew, lmul, avl);
+                test_vmacc_vv(sew, lmul, avl);
+
                 test_vwmaccu_vv(sew, lmul, avl);
+                test_vwmaccu_vx(sew, lmul, avl);
+
+                test_vwmacc_vv(sew, lmul, avl);
+                test_vwmacc_vx(sew, lmul, avl);
+
+                test_vwmaccsu_vv(sew, lmul, avl);
+                test_vwmaccsu_vx(sew, lmul, avl);
+                test_vwmaccus_vx(sew, lmul, avl);
             }
         }
     }
