be6bd958f565e7f416c10d6bb88ca023bbbf43ed
Joii <joii@cryptape.com>
2022-06-16 16:00:23 +0800

    Revert "Optimize random number generation"
    This reverts commit 35072b0a2c7d05768a1a981e4ec1dfe69b5787b9.

--- cases/src/rng.rs
+++ cases/src/rng.rs
@@ -1,14 +1,13 @@
-use super::{misc::set_bit_in_slice, rng_data::G_DATA};
-use alloc::vec::Vec;
+use super::rng_data::G_DATA;
 
-use super::log;
-use ckb_std::syscalls::debug;
+// use super::log;
+// use ckb_std::syscalls::debug;
 
 static mut SEED: usize = 0;
 
 pub fn set_seed(seed: usize) {
     unsafe {
-        SEED = seed % G_DATA.len();
+        SEED = seed;
     }
 }
 
@@ -33,61 +32,4 @@ pub fn fill_rand_bytes(dest: &mut [u8]) {
     }
 
     set_seed(seed);
-}
-
-fn gen_rand_u8() -> u8 {
-    let seed = get_seed();
-    let ret = G_DATA[seed];
-    set_seed(seed + 1);
-    ret
-}
-
-pub fn fill_rand_mask(dest: &mut [u8]) {
-    let mut count = 0;
-    let mut val = 0;
-    for i in 0..dest.len() * 8 {
-        if count == 0 {
-            count = gen_rand_u8() % 32 + 1;
-            val = if val == 1 { 0 } else { 1 }
-        }
-        set_bit_in_slice(dest, i, val);
-        count -= 1
-    }
-}
-
-pub fn fill_rand_sew(dest: &mut [u8], sew: u64) {
-    let sew = sew as usize;
-    let sew_byte = sew / 8;
-    let mut zero_data = Vec::<u8>::new();
-    zero_data.resize(sew_byte, 8);
-    for i in 0..dest.len() / sew_byte {
-        let v = gen_rand_u8();
-        let t = v % 10;
-        if t == 0 {
-            // zero
-            dest[i * sew_byte..(i + 1) * sew_byte].copy_from_slice(&zero_data);
-        } else {
-            let mut data = Vec::<u8>::new();
-            data.resize(sew_byte, 0);
-            fill_rand_bytes(&mut data);
-            if t == 1 {
-                let z = (v as usize >> 2) % (sew / 2) + 1;
-                for i in 0..z {
-                    set_bit_in_slice(&mut data, i, 0);
-                }
-            } else if t == 3 {
-                let z = (v as usize >> 2) % (sew / 2) + 1;
-                for i in z..(data.len() * 8) {
-                    set_bit_in_slice(&mut data, i, 0);
-                }
-            } else if t  == 5 {
-                let z = (v as usize >> 2) % (sew / 2) + 1;
-                for i in z..(data.len() * 8) {
-                    set_bit_in_slice(&mut data, i, 1);
-                }
-            }
-            dest[i * sew_byte..(i + 1) * sew_byte].copy_from_slice(&data);
-        }
-    }
-    //log!("--rand data: \n{:0>2X?}", dest);
 }
